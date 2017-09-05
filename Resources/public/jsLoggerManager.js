46445de39a84f9e7596e8a41d50baa0c2357fe71
Charly BIGO <c.bigo@waigeo.fr>
2017-09-05 16:44:36 +0200

    Ajout de l'heure sur la date + Affichage userFriendly du navigateur et de la version

--- Resources/public/jsLoggerManager.js
+++ Resources/public/jsLoggerManager.js
@@ -1,7 +1,19 @@
 var jsLoggerManager = {
 
-    init: function(saveLogErrorUrl){
-        window.onerror = function(messageOrEvent, source, lineno, colno, error){
+    init: function (saveLogErrorUrl) {
+        window.onerror = function (messageOrEvent, source, lineno, colno, error) {
+            var message = "Impossible de récupérer le message de l'erreur originelle.";
+
+            if(messageOrEvent != null){
+                if(typeof(messageOrEvent) == "string"){
+                    message = messageOrEvent;
+                }
+
+                if(typeof(messageOrEvent) == "object"){
+                    message = JSON.stringify(messageOrEvent);
+                }
+            }
+
             $.ajax(saveLogErrorUrl, {
                 type: "GET",
                 data: {
@@ -9,9 +21,25 @@ var jsLoggerManager = {
                     url: source,
                     lineNumber: lineno,
                     colNumber: colno,
-                    userAgent: navigator.userAgent
+                    userAgent: jsLoggerManager.getNavigator()
                 }
             });
         };
+    },
+
+    getNavigator: function () {
+        var ua = navigator.userAgent, tem,
+            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE ' + (tem[1] || '');
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return M.join(' ');
     }
 };
--- Resources/public/logsManager.js
+++ Resources/public/logsManager.js
@@ -9,7 +9,7 @@ MyDateField.prototype = new jsGrid.Field({
 
         var date = new Date(value.timestamp * 1000);
 
-        return ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
+        return ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear() + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
     }
 });
 
